07e7aa283c80fafa9e510dc5a394e31b47361f0e
lievan <0evanli0@gmail.com>
2022-04-20 14:04:48 -0400

    added articles,not formatted

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,20 +1,105 @@
 import React from "react";
 import styled from "styled-components";
-import NormalArticle from "./NormalArticle";
-import SideArticle from "./SideArticle";
-import useWindowDimensions from "./windowDimensions";
-const Article = ({ article }) => {
-  const { height, width } = useWindowDimensions();
 
-  if (width > "768") {
-    if (article.type === "normal") {
-      return <NormalArticle article={article} />;
-    } else if (article.type === "left") {
-      return <SideArticle article={article} left={true} />;
-    } else {
-      return <SideArticle article={article} left={false} />;
-    }
+import { device } from "../device";
+
+const ArticleWrap = styled.div`
+  width: 586px;
+  height: 714px;
+  left: 246px;
+  top: 4506px;
+  margin: 30px;
+  background: #E9C898;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  align-items: center;
+  text-align: center;
+  @media ${device.mobile} {
+    width: 95vw;
   }
-  return <NormalArticle article={article} />;
+`;
+const Wrap = styled.div`
+  position: relative;
+  margins: 10px;
+  width: 90%;
+  height: 90%;
+  left: 5%;
+  background: #FFFFFF;
+  text-align: center;
+`
+
+const Link = styled.a`
+  text-decoration: none;
+  width: inherit;
+`;
+
+const ArticleImg = styled.img`
+  width: 386px;
+  height: 242px;
+  background: #C4C4C4;
+  left: 13%;
+  top: 8%;
+  position: absolute;
+  @media ${device.mobile} {
+    width: 100%;
+    height: 60vw;
+  }
+`;
+
+
+const ArticleText = styled.p`
+  text-align: center;
+  align-items: center;
+  top: 40%;
+  position: absolute;
+  text-align: center;
+`
+
+const Title = styled.p`
+  font-family: 'Playfair Display';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  color: #313131;
+  margin-bottom: 0px;
+`
+
+const Author = styled.p`
+  font-family: 'Playfair Display';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  text-align: center;
+  color: #313131;
+`;
+
+
+const Details = styled.p`
+  font-family: 'Raleway';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  text-align: center;
+  color: #000000;
+  margin: 10px;
+`
+
+
+const Article = ({ article }) => {
+  return (
+    <ArticleWrap>
+      <Wrap>
+        <Link href={article.link} target="_blank" rel="noreferrer">
+          <ArticleImg src={article.img} />
+          <ArticleText>
+            <Title>{article.title}</Title>
+            <Author>{article.author}</Author>
+            <Details>{article.description}</Details>
+          </ArticleText>
+        </Link>
+      </Wrap>
+    </ArticleWrap>
+  );
 };
+
 export default Article;
